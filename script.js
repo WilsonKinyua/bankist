a89b53bbf18a96707690725306ced355b5336203
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-11 00:30:52 +0300

    selecting, creating and deleting dom elements

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
+
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+// =================== create, select, delete dom ====================
+// console.log(document.documentElement)
+
+// const allSections = document.querySelectorAll(".section");
+
+// console.log(allSections)
+// const header = document.querySelector(".header");
+
+// const message = document.createElement('div');
+
+// message.classList.add("cookie-message");
+// message.innerHTML = "We use cookies for modern browsers. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// header.append(message);
+
+// const btnClose = document.querySelector(".btn--close-cookie");
+
+// btnClose.addEventListener("click", () => {
+//   message.remove();
+// });
