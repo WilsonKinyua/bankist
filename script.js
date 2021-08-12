fe284174eb1de89c3c2e24876f2255bf9ef87e91
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-12 13:04:49 +0300

    smooth scroll form navigation bar

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
+const btnToScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -19,8 +21,7 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
-
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
@@ -31,6 +32,38 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+btnToScrollTo.addEventListener("click", function () {
+  const sec1coord = section1.getBoundingClientRect();
+  console.log(sec1coord);
+
+  // method 1======>>>>>
+  // scroll to specified section
+  // window.scrollTo(
+  //   sec1coord.left + window.pageXOffset,
+  //   sec1coord.top + window.pageYOffset
+  // );
+
+  // method 2======>>>>>
+  // window.scrollTo({
+  //   left: sec1coord.left + window.pageXOffset,
+  //   top: sec1coord.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  // method 3======>>>>>
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// ====================== page navigation to scroll down ==================
+
+document.querySelectorAll(".nav__link").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
 
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
@@ -57,14 +90,14 @@ document.addEventListener("keydown", function (e) {
 //   message.remove();
 // });
 
-
 //======================= styles, attributes, classes ===============================
 const header = document.querySelector(".header");
 
-const message = document.createElement('div');
+const message = document.createElement("div");
 
 message.classList.add("cookie-message");
-message.innerHTML = "We use cookies for modern browsers. <button class='btn btn--close-cookie'>Got it!</button>";
+message.innerHTML =
+  "We use cookies for modern browsers. <button class='btn btn--close-cookie'>Got it!</button>";
 
 header.append(message);
 
@@ -74,12 +107,39 @@ btnClose.addEventListener("click", () => {
   message.remove();
 });
 
-message.style.backgroundColor = "#37383d";
-// message.style.width = "120%";
+// message.style.backgroundColor = "#37383d";
+// // message.style.width = "120%";
 
-document.documentElement.style.setProperty('--color-primary', 'yellow');
+// document.documentElement.style.setProperty('--color-primary', 'yellow');
 
-// attributes
-const logo = document.querySelector(".nav__logo");
-logo.src = "https://cdn.hubilo.com/logo/1402283/300/1596_9068_763672001621839178.jpg";
-console.log(logo.src);
+// // attributes
+// const logo = document.querySelector(".nav__logo");
+// logo.src = "https://cdn.hubilo.com/logo/1402283/300/1596_9068_763672001621839178.jpg";
+// console.log(logo.src);
+
+// event handlers
+// const h1 = document.querySelector("h1");
+// window.addEventListener("online", function(e){
+//   alert("You are now connected to the network.");
+// });
+
+// generate random color
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// // console.log(randomColor(0,255))
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
